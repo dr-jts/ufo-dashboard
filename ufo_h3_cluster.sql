1d389ea7c850d3718d4d39139eed86e2f909fffa
Martin Davis <mtnclimb@gmail.com>
2024-11-18 12:44:36 -0800

    Update ufo_h3_cluster.sql

--- ufo_h3_cluster.sql
+++ ufo_h3_cluster.sql
@@ -5,11 +5,11 @@ FUNCTION public.us_ufo_h3_cluster(z integer, x integer, y integer)
 RETURNS bytea
 AS $$
 WITH 
--- Compute the tile envelope to avoid repeated calculations
+-- Pre-calculate the tile envelope to avoid repeated calculations
 tile_env AS (
-    SELECT ST_TileEnvelopeClip(z, x, y, margin => 0.125) AS env_geom
+    SELECT ST_Transform(ST_TileEnvelopeClip(z, x, y, margin => 0.125), 4326) AS env_geom
 ),
--- Compute the H3 resolution based on zoom level
+-- Calculate the H3 resolution based on zoom level
 resolution AS (
     SELECT CASE
         WHEN z <= 2 THEN 2
@@ -26,7 +26,7 @@ cell AS (
         h3_lat_lng_to_cell(geom, r.h3_res) AS cellid
 	FROM us_ufo
     CROSS JOIN resolution r
-	WHERE ST_Intersects(geom, ST_Transform((SELECT env_geom FROM tile_env), 4326))
+	WHERE ST_Intersects(geom, (SELECT env_geom FROM tile_env))
     GROUP BY cellid
 ), 
 -- Tile bounds in Web Mercator (3857)
