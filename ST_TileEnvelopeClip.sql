269bf7e6bd9d5346170a737962c533716bfa267e
Martin Davis <mtnclimb@gmail.com>
2024-11-18 12:20:14 -0800

    Update ST_TileEnvelopeClip.sql

--- ST_TileEnvelopeClip.sql
+++ ST_TileEnvelopeClip.sql
@@ -1,4 +1,4 @@
--- workaround for margin-clipping bug in ST_TileEnvelope function
+-- workaround for margin-clipping bug in ST_TileEnvelope function (see https://trac.osgeo.org/postgis/ticket/5799)
 CREATE OR REPLACE FUNCTION ST_TileEnvelopeClip(zoom integer, x integer, y integer, 
     bounds geometry 
               DEFAULT 'SRID=3857;LINESTRING(-20037508.342789244 -20037508.342789244, 20037508.342789244 20037508.342789244)'::geometry, 
